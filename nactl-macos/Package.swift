d99aa571358d353fcbf1b521ad47e183da52101f
Forrester Terry <fterry@stanford.edu>
2026-01-28 19:03:02 -0800

    Pin swift-argument-parser dependency to 1.3.1

--- nactl-macos/Package.swift
+++ nactl-macos/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "nactl",
     platforms: [.macOS(.v12)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.3.1"),
     ],
     targets: [
         .executableTarget(
